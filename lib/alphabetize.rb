95180820cb37d275530731af2c4ca320a2008bd2
Roniece Ricardo <roniecericardo@gmail.com>
2018-05-14 03:08:11 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,6 @@
 def alphabetize(arr)
-  # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |word| #for each word in the array
+    word.chars.map {|e| esperanto_alphabet.index(e)} #sorts by the index of e in alphabet
+  end
 end
